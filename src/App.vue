d48a05a8932a8abbd70652d9e381e64f0e1fa2b5
darkopokoracki <darkopokoracki@gmail.com>
2021-11-11 19:36:41 +0100

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <UserProfile/>
+  </div>
+</template>
+
+<script>
+import UserProfile from "./components/UserProfile.vue";
+
+export default {
+  name: 'App',
+  components: { UserProfile }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  background-color: #f3f5fa;
+}
+</style>
